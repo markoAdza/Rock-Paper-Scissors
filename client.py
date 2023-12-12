2ab01cce080f96ced1d0d9b6bfedeb8d11bc8810
Miha Krajnc <miha.krajnc@dewesoft.com>
2023-12-12 17:48:43 +0100

    add basic networking

---
+++ client.py
@@ -0,0 +1,82 @@
+from network import Network
+import pickle
+
+def main():
+    run = True
+    clock = pygame.time.Clock()
+    n = Network()
+    player = int(n.getP())
+    print("You are player", player)
+
+    while run:
+        clock.tick(60)
+        try:
+            game = n.send("get")
+        except:
+            run = False
+            print("Couldn't get game")
+            break
+
+        if game.bothWent():
+            redrawWindow(win, game, player)
+            pygame.time.delay(500)
+            try:
+                game = n.send("reset")
+            except:
+                run = False
+                print("Couldn't get game")
+                break
+
+            font = pygame.font.SysFont("comicsans", 90)
+            if (game.winner() == 1 and player == 1) or (game.winner() == 0 and player == 0):
+                text = font.render("You Won!", 1, (255,0,0))
+            elif game.winner() == -1:
+                text = font.render("Tie Game!", 1, (255,0,0))
+            else:
+                text = font.render("You Lost...", 1, (255, 0, 0))
+
+            win.blit(text, (width/2 - text.get_width()/2, height/2 - text.get_height()/2))
+            pygame.display.update()
+            pygame.time.delay(2000)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                pygame.quit()
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pos = pygame.mouse.get_pos()
+                for btn in btns:
+                    if btn.click(pos) and game.connected():
+                        if player == 0:
+                            if not game.p1Went:
+                                n.send(btn.text)
+                        else:
+                            if not game.p2Went:
+                                n.send(btn.text)
+
+        redrawWindow(win, game, player)
+
+def menu_screen():
+    run = True
+    clock = pygame.time.Clock()
+
+    while run:
+        clock.tick(60)
+        win.fill((128, 128, 128))
+        font = pygame.font.SysFont("comicsans", 60)
+        text = font.render("Click to Play!", 1, (255,0,0))
+        win.blit(text, (100,200))
+        pygame.display.update()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                run = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                run = False
+
+    main()
+
+while True:
+    menu_screen()
--- main.py
+++ main.py
@@ -5,97 +5,145 @@ import cvzone
 from cvzone.HandTrackingModule import HandDetector
 import time
 import random
+from network import Network
+from game import Game
+import sys
+import numpy as np
 
+def other_id(id):
+    if id == 0:
+        return 1
+    else:
+        return 0
 
-cap = cv2.VideoCapture(0)
-cap.set(3, 640)
-cap.set(4, 480)
+useCam = not ('nocam' in sys.argv)
 
-detector = HandDetector(maxHands=1)
+cap = None
+detector = None
+if useCam:
+    cap = cv2.VideoCapture(0)
+    cap.set(3, 640)
+    cap.set(4, 480)
+
+    detector = HandDetector(maxHands=1)
 
 timer = 0
-stateResult = False
-startGame = False
+countingDown = False
 scores = [0,0]
 
-while True:
-    imgBG = cv2.imread('Resources/BG.png')
-    SUCCESS, img = cap.read()
+gameType = int(input("Select game type (AI 1, CONNECT 2): "))
+offlineGame = Game
+n = None
+imgAI = []
+id = 1
+if gameType == 1:
+    offlineGame = Game(0)
+    offlineGame.ready = True
+elif gameType == 2:
+    n = Network(input("Specify server ip address: "))
+    id = int(n.getP())
 
-    imgScaled = cv2.resize(img, (0,0), None, 0.875, 0.875)
-    imgScaled = imgScaled[:, 80:480]
+while True:
+    if n == None:
+        game = offlineGame
+    else:
+        game = n.send("get")
+
+    imgBG = cv2.imread('Resources/BG.png')
+    imgScaled = np.zeros((420,400,4), np.uint8)
+    camImg = None
+    if useCam:
+        SUCCESS, img = cap.read()  
+        camImg = cv2.resize(img, (420, 400))
+        img = cv2.cvtColor(img, cv2.COLOR_RGB2RGBA) 
+        imgScaled = cv2.resize(img, (276, 276))
+    elif (id == 0 and game.p1Went) or (id == 1 and game.p2Went):
+        imgMap = {'R': 1, 'P':2, 'S': 3}
+        img = cv2.imread(f'Resources/{imgMap[game.moves[id]]}.png', cv2.IMREAD_UNCHANGED)
+        imgScaled = cv2.resize(img, (276, 276))
+
 
     # find hands
-    hands, img = detector.findHands(imgScaled)
-    if hands:
-        playerMove = None
-        hand = hands[0]
-        fingers = detector.fingersUp(hand)
-        if fingers == [0, 0, 0, 0, 0]: # rock
-            playerMove = 1
-            cv2.putText(imgBG, "ROCK", (955,700),cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 255), 4)
-        if fingers == [1, 1, 1, 1, 1]: # paper
-            playerMove = 2
-            cv2.putText(imgBG, "PAPER", (955,700),cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 255), 4)
-        if fingers == [0, 1, 1, 0, 0]: # scissors
-            playerMove = 3
-            cv2.putText(imgBG, "SCISSORS", (955,700),cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 255), 4)
+    hands = None
+    playerMove = None
+    if useCam:
+        hands, img = detector.findHands(camImg)
+        if hands:
+            hand = hands[0]
+            fingers = detector.fingersUp(hand)
+            if fingers == [0, 0, 0, 0, 0]: # rock
+                playerMove = 'R'
+                cv2.putText(imgBG, "ROCK", (955,700),cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 255), 4)
+            if fingers == [1, 1, 1, 1, 1]: # paper
+                playerMove = 'P'
+                cv2.putText(imgBG, "PAPER", (955,700),cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 255), 4)
+            if fingers == [0, 1, 1, 0, 0]: # scissors
+                playerMove = 'S'
+                cv2.putText(imgBG, "SCISSORS", (955,700),cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 255), 4)
+    else:
+        playerMove = cv2.waitKey(1)
+        if (playerMove) == ord('r'):
+            playerMove = 'R'
+        elif (playerMove) == ord('p'):
+            playerMove = 'P'
+        elif (playerMove) == ord('s'):
+            playerMove = 'S'
+        else:
+            playerMove = None
 
-    if startGame:
+    if countingDown:
+        timer = time.time() - initialTime
+        cv2.putText(imgBG, str(int(timer)), (605,435),cv2.FONT_HERSHEY_PLAIN, 6, (255, 0, 255), 4)
 
-        if stateResult is False:
-            timer = time.time() - initialTime
-            cv2.putText(imgBG, str(int(timer)), (605,435),cv2.FONT_HERSHEY_PLAIN, 6, (255, 0, 255), 4)
+        if timer > 3:
+            countingDown = False
 
-            if timer > 3:
-                stateResult = True
-                timer = 0
+            if n == None:
+                game.play(other_id(id), 'RPS'[random.randint(0, 2)])
 
-                if hands:
-                    playerMove = None
-                    hand = hands[0]
-                    fingers = detector.fingersUp(hand)
-                    if fingers == [0, 0, 0, 0, 0]:
-                        playerMove = 1
-                    if fingers == [1, 1, 1, 1, 1]:
-                        playerMove = 2
-                    if fingers == [0, 1, 1, 0, 0]:
-                        playerMove = 3
+            if game.bothWent():
+                if game.winner() == -1:
+                    print("tie!")
+                elif game.winner() == id:
+                    print("win!")
+                    scores[id]+=1
+                else:
+                    scores[other_id(id)]+=1
 
+            moveNum = 1
+            if game.moves[other_id(id)] == 'P':
+                moveNum = 2
+            elif game.moves[other_id(id)] == 'S':
+                moveNum = 3
 
-                    randomNumber = random.randint(1, 3)
+            imgAI = cv2.imread(f'Resources/{moveNum}.png', cv2.IMREAD_UNCHANGED)            
 
-                    imgAI = cv2.imread(f'Resources/{randomNumber}.png', cv2.IMREAD_UNCHANGED)
-                    imgBG = cvzone.overlayPNG(imgBG, imgAI, (149, 310))
+    if game.connected() and (playerMove != None):
+        if n == None:
+            if not game.p1Went:
+                game.play(id, playerMove)
+        elif (n != None) and (id == 0 and not game.p1Went) or (id == 1 and not game.p2Went):
+            n.send(playerMove)
 
-                    # Player wins
-                    if (playerMove == 1 and randomNumber == 3) or \
-                        (playerMove == 2 and randomNumber == 1) or \
-                        (playerMove == 3 and randomNumber == 2):
-                        scores[1] +=1
-                    
-                    # AI wins
-                    if (playerMove == 3 and randomNumber == 1) or \
-                        (playerMove == 1 and randomNumber == 2) or \
-                        (playerMove == 2 and randomNumber == 3):
-                        scores[0] +=1
+    imgBG = cvzone.overlayPNG(imgBG, imgScaled, (860, 310))
 
-    imgBG[234:654, 795:1195] = imgScaled
-
-    if stateResult:
+    if len(imgAI) > 0:
         imgBG = cvzone.overlayPNG(imgBG, imgAI, (149, 310))
 
     cv2.putText(imgBG, str(scores[0]), (410,215),cv2.FONT_HERSHEY_PLAIN, 4, (255, 225, 255), 6)
     cv2.putText(imgBG, str(scores[1]), (1112,215),cv2.FONT_HERSHEY_PLAIN, 4, (255, 225, 255), 6)
 
-
-   
-
     # cv2.imshow('image', img)
     cv2.imshow('BG', imgBG)
     # cv2.imshow('Scaled', imgScaled)
     key = cv2.waitKey(1)
-    if key == ord('s'):
-        startGame = True
+    if key == ord('b'):
+        imgAI = []
+        countingDown = True
         initialTime = time.time()
-        stateResult = False
+
+        if n == None:
+            offlineGame = Game(0)
+            offlineGame.ready = True
+        else:
+            game = n.send("reset")
